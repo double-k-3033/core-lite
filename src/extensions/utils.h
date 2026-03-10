7bc1ea87ba4cd85b656dc8690a770be6de6f05b0
feiyu <lvp123321123321@gmail.com>
2026-03-10 22:41:15 +0700

    try fix compile error in older clang

--- src/extensions/utils.h
+++ src/extensions/utils.h
@@ -17,15 +17,16 @@ static std::string wchar_to_string(const wchar_t* wstr) {
         return std::string();
     }
 
-    std::u16string u16str;
-    for (size_t i = 0; wstr[i]; i++) {
-        uint16_t c = static_cast<uint16_t>(wstr[i]); // truncate to 16-bit
-        u16str += c;
-    }
+    std::string result;
+    size_t len = 0;
 
-    // Convert UTF-16 to UTF-8
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
-    return convert.to_bytes(u16str);
+    while (wstr[len] != L'\0') len++;
+
+    result.reserve(len);
+    for (size_t i = 0; i < len; i++) {
+        result.push_back(static_cast<char>(wstr[i]));
+    }
+    return result;
 }
 
 #ifdef _MSC_VER
