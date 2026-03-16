1a085274030413cb664b5358e9eba1f887ec3dfa
feiyu <lvp123321123321@gmail.com>
2026-03-16 16:21:24 +0700

    fix windows build

--- src/extensions/overload.h
+++ src/extensions/overload.h
@@ -932,8 +932,8 @@ struct Overload {
             int opt = 1;
             int buf_size = 16 * 1024 * 1024; // 16MB
             setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt));
-            setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size));
-            setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &buf_size, sizeof(buf_size));
+            setsockopt(sock, SOL_SOCKET, SO_RCVBUF, (char*)&buf_size, sizeof(buf_size));
+            setsockopt(sock, SOL_SOCKET, SO_SNDBUF, (char*)&buf_size, sizeof(buf_size));
             if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
                 logToConsole(L"Failed to bind socket!");
                 closesocket(sock);
@@ -1008,8 +1008,8 @@ struct Overload {
             tcpData->connectStatus = ConnectStatus::Connected;
 
             int buf_size = 16 * 1024 * 1024; // 16MB
-            setsockopt(clientSocket, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size));
-            setsockopt(clientSocket, SOL_SOCKET, SO_SNDBUF, &buf_size, sizeof(buf_size));
+            setsockopt(clientSocket, SOL_SOCKET, SO_RCVBUF, (char*)&buf_size, sizeof(buf_size));
+            setsockopt(clientSocket, SOL_SOCKET, SO_SNDBUF, (char*)&buf_size, sizeof(buf_size));
 
             if (peer)
             {
